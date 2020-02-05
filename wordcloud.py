b2cf7ca96924aa30fd992e29b0426bd191d99ba5
ellelenlee <47921477+ellelenlee@users.noreply.github.com>
2020-02-05 18:53:45 +0800

    Update wordcloud.py

--- wordcloud.py
+++ wordcloud.py
@@ -9,9 +9,24 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from PIL import Image
 
-Image.new("RGB" ,(512,512),(0,0,255)).save("wuhan.png","PNG")
+Image.new("RGB" ,(512,512),(0,0,255)).save("武汉.png","PNG")
 
-stopword=['微博', '我们', '全文', '...',  '视频', '真的']
+if __name__ == '__main__':
+    keyword = '武汉'
+    mblogs = json.loads(open('{}.json'.format(keyword), 'r', encoding='utf-8').read())
+    print('微博总数：', len(mblogs))
+
+    words = []
+    for blog in mblogs:
+        words.extend(jieba.analyse.extract_tags(blog['text']))
+    print("总词数：", len(words))
+
+word_counts = collections.Counter(words) 
+word_counts_top100 = word_counts.most_common(100) 
+print (word_counts_top100)
+
+
+stopword=['微博', '我们', '全文', '...',  '视频', '真的', '分享', '网页', '链接']
 
 def gen_img(texts, img_file):
     data = ' '.join(text for text in texts)
@@ -25,26 +40,11 @@ def gen_img(texts, img_file):
         font_path='/Library/Fonts/STHeiti Light.ttc'
     )
     wc.generate(data)
+    wc.to_file(img_file.split('.')[0] + '_wc.png')
+    
+gen_img(words, '武汉.png')
 
 #     plt.figure()
 #     plt.imshow(wc, interpolation="bilinear")
 #     plt.axis("off")
 #     plt.show()
-
-    wc.to_file(img_file.split('.')[0] + '_wc.png')
-
-if __name__ == '__main__':
-    keyword = '武汉'
-    mblogs = json.loads(open('result_{}.json'.format(keyword), 'r', encoding='utf-8').read())
-    print('微博总数：', len(mblogs))
-
-    words = []
-    for blog in mblogs:
-        words.extend(jieba.analyse.extract_tags(blog['text']))
-    print("总词数：", len(words))
-
-word_counts = collections.Counter(words) 
-word_counts_top10 = word_counts.most_common(10) 
-print (word_counts_top10)
-    
-gen_img(words, 'wuhan.png')
